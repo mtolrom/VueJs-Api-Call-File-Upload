4229aa9fa459f0dc69c8f08f7c93f2cdbab745ac
mtolrom <SDE2013@outlook.com>
2019-02-06 21:14:04 -0800

    upload returning status:201, statusText:Created

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
         {{$session.get('myI')}}
         <br /><br />
         {{ infos }}
-        <h2 style="font-family:Fahkwang;">Upload a File - Vuejs API Call</h2>
+        <h2>Upload a File - Vuejs API Call</h2>
         <form enctype="multipart/form-data">
             <div class="form-horizontal">
                 <div class="form-group">
-                    <p class="control-label col-md-2" style="font-family:Fahkwang;">Language</p>
+                    <p class="control-label col-md-2">Language</p>
                     <div class="col-md-10">
                         <select class="form-control" style="width:80%;" name="partitionkey">
                             <option value="English">English</option>
@@ -19,46 +19,46 @@
                     </div>
                 </div>
                 <div class="form-group">
-                    <p class="control-label col-md-2" style="font-family:Fahkwang;">Book Title</p>
+                    <p class="control-label col-md-2">Book Title</p>
                     <div class="col-md-10">
                         <input type="text" name="title" class="form-control" style="width:80%;" maxlength="200">
                     </div>
                 </div>
                 <div class="form-group">
-                    <p class="control-label col-md-2" style="font-family:Fahkwang;">Book Text</p>
+                    <p class="control-label col-md-2">Book Text</p>
                     <div class="col-md-10">
                         <textarea rows="5" class="form-control" style="width:80%;" name="body" maxlength="2000"></textarea>
                     </div>
                 </div>
                 <div class="form-group">
-                    <p class="control-label col-md-2" style="font-family:Fahkwang;">Images</p>
+                    <p class="control-label col-md-2">Images</p>
                     <div class="col-md-10">
                         <input type="file" multiple="multiple" id="photoUrl" name="file" v-on:change="fileChange($event.target.files)" accept="application/vnd.msword, application/vnd.ms-excel, application/vnd.ms-powerpoint,
                 text/plain, application/pdf, image/*">
                     </div>
                 </div>
                 <div class="form-group">
-                    <p class="control-label col-md-2" style="font-family:Fahkwang;">Sounds</p>
+                    <p class="control-label col-md-2">Sounds</p>
                     <div class="col-md-10">
                         <input type="file" multiple="multiple" id="photoUrl" name="file" v-on:change="fileChange($event.target.files)" accept="application/vnd.msword, application/vnd.ms-excel, application/vnd.ms-powerpoint,
                 text/plain, application/pdf, image/*">
                     </div>
                 </div>
                 <div class="form-group">
-                    <p class="control-label col-md-2" style="font-family:Fahkwang;">Book Author</p>
+                    <p class="control-label col-md-2">Book Author</p>
                     <div class="col-md-10">
                         <input type="text" name="custom5" class="form-control" style="width:80%;" maxlength="100">
                     </div>
                 </div>
                 <div class="form-group">
-                    <p class="control-label col-md-2" style="font-family:Fahkwang;">Tags</p>
+                    <p class="control-label col-md-2">Tags</p>
                     <div class="col-md-10">
                         <input type="text" name="custom3" class="form-control" style="width:80%;" maxlength="500">
                     </div>
                 </div>
             </div>
             <div class="form-group">
-                <button type="button" v-on:click="created()" class="btn btn-primary btn-lg" style="font-family:Fahkwang;width:25%;">Submit</button>
+                <button type="button" v-on:click="created()" class="btn btn-primary btn-lg" style="width:25%;">Submit</button>
             </div>
         </form>
     </div>
@@ -89,7 +89,7 @@
                 .then(response => {this.$session.set('myI',response.data.token);})
                 .catch(error => (this.info = error))
               var myDate = new Date();
-              var myUrl = "https://demofunctionsmikal201901.blob.core.windows.net/getsastest/myFileMekone.jpg"+this.$session.get('myI');
+              var myUrl = "https://ad440oneboxtempbb81.blob.core.windows.net/getsastoken/myFileMekone.jpg"+this.$session.get('myI');
               axios({ method: "PUT",
                   "url": myUrl,
                   "data": this.files,
@@ -108,10 +108,11 @@
 </script>
 
 <style>
-    #app {
-    padding: 20px;
-    }
     @import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
     @import "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
-    @import "https://fonts.googleapis.com/css?family=Roboto|Josefin Sans|Quicksand|Julius Sans One|Niconne|Marcellus SC|Montserrat|Poiret One|EB Garamond|Forum|Arsenal|Parisienne|Fahkwang|Marmelad";
+    @import "https://fonts.googleapis.com/css?family=Roboto|Parisienne|Fahkwang|Marmelad";
+    #app {
+      padding: 20px;
+      font-family: Marmelad;
+    }
 </style>
